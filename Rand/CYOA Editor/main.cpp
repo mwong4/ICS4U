9fb709d28a78ef09bb55d709d06562e35710ae93
Max <wong.max.c@gmail.com>
2020-11-13 12:06:46 -0500

    Implemented basic data storage infrastructure

--- Rand/CYOA Editor/main.cpp
+++ Rand/CYOA Editor/main.cpp
@@ -1,17 +1,37 @@
 /*
 >- Author: Max Wong
 >- Date: November 11, 2020
->- Updated: Nov 11, 2020
+>- Updated: Nov 13, 2020
 >- Purpose: To write a cyoa editor -> to advance coding knowledge with more general case infrastructure
 
 To Do
 prototype creating txt files
 
+data save/load
+add page
+display page hierarchy
+
 */
 
 #include <iostream>
+#include <vector>
 
 using namespace std;
+
+struct Options //This stores an option on a page
+{
+    string text; //This is the text displayed for the option
+    string link; //This will be used in to link to the next page
+    string rand; //temporary spot for adding an operation later on
+};
+
+struct Page //This stores the page information for each option
+{
+    string title; //This is the title of the page
+    string text; //This is tht text displayed for the page
+    int optionCount; //This int represents the # of options
+    vector<Options> options; //Keep a vector of options
+};
 
 int main()
 {
