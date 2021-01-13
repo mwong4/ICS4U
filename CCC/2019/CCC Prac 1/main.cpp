785ecdc5db9ace84ddb52271453766791b64066a
Max <wong.max.c@gmail.com>
2021-01-12 22:17:23 -0500

    2019 q1 done

---
+++ CCC/2019/CCC Prac 1/main.cpp
@@ -0,0 +1,81 @@
+#include <iostream>
+
+using namespace std;
+
+/*
+0 1
+2 3
+*/
+
+void vertFlip(int _pos[])
+{
+    int storeVal;
+    //Swap top
+    storeVal = _pos[1];
+    _pos[1] = _pos[0];
+    _pos[0] = storeVal;
+    //Swap bottom
+    storeVal = _pos[3];
+    _pos[3] = _pos[2];
+    _pos[2] = storeVal;
+    return;
+}
+
+void horiFlip(int _pos[])
+{
+    int storeVal;
+    //Swap left
+    storeVal = _pos[2];
+    _pos[2] = _pos[0];
+    _pos[0] = storeVal;
+    //Swap right
+    storeVal = _pos[3];
+    _pos[3] = _pos[1];
+    _pos[1] = storeVal;
+    return;
+}
+
+int main()
+{
+    string input;
+
+    string tempString;
+    int hFlip = 0;
+    int vFlip = 0;
+    int pos[4] = {1, 2, 3, 4};
+
+    input = "";
+    cin >> input;
+
+    //Break up input
+    for(int i = (input.size() - 1); i >= 0; i --)
+    {
+        //get character at the back
+        tempString = input.at(i);
+
+        if(tempString == "H")
+        {
+            hFlip ++;
+        }
+        else if(tempString == "V")
+        {
+            vFlip ++;
+        }
+    }
+
+    if(hFlip % 2 == 1)
+    {
+        horiFlip(pos);
+    }
+
+    if(vFlip % 2 == 1)
+    {
+        vertFlip(pos);
+    }
+
+    //output
+    cout << pos[0] << " " << pos[1] << endl;
+    cout << pos[2] << " " << pos[3] << endl;
+
+    return 0;
+}
