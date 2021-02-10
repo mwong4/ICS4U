ed067cf43101cc5cc23815da267f5e49e7079b08
Max <wong.max.c@gmail.com>
2021-02-10 12:05:34 -0500

    Finished Sorting Assignment

--- Assignment/SelectionSortingExplained/main.cpp
+++ Assignment/SelectionSortingExplained/main.cpp
@@ -1,22 +1,21 @@
 /*
 Author: Max Wong
 Date Created:Feb 9, 2020
-Date Updated:Feb 9, 2020
+Date Updated:Feb 10, 2020
 Purpose: Show selecion sorting
 
 Source
 Error Trapping from ICS3U projects by Max Wong
 Color idea from Stack Overflow Forum: https://stackoverflow.com/questions/4053837/colorizing-text-in-the-console-with-c
-
-TODO
-
-Improved comments
-Legend
+Srand based on time from Cplusplus.com: http://www.cplusplus.com/reference/cstdlib/srand/
 
 */
 #include <iostream>
 #include <limits>        //For error trapping
-#include <windows.h>       //For system commands
+#include <windows.h>     //For system commands
+#include <stdlib.h>      //For srand
+#include <time.h>        //For time
+#include <stdio.h>       //For Null
 
 using namespace std;
 
@@ -32,11 +31,13 @@ void printLegend(); //Used for printing the color legend
 
 int main()
 {
+    srand(time(NULL)); //Srand based on time
+
     string options[1] = {"Run sort"}; //list of options for menu
     int input; //Input from user
 
-    int elements[9]; //Array being sorted
-    const int arraySize = 9; //Length of the array
+    int elements[8]; //Array being sorted
+    const int arraySize = 8; //Length of the array
 
     do
     {
@@ -48,6 +49,8 @@ int main()
 
         if(input == 1)
         {
+            printLegend(); //Call function to print legend
+            cout << endl;
             selectionSort(elements, arraySize); //Call function to do the selection sort
         }
 
@@ -72,42 +75,38 @@ void selectionSort(int _elements[], int _size)
         smallestPos = i;
         for(int j = i; j < _size; j++) //Go through and find smallest
         {
-            printArray(_elements, _size, "//Search for smallest", i, smallestPos, j, 0, 0);
+            printArray(_elements, _size, "//Comparing", i-1, smallestPos, j, 0, 0); //Call function to display array
             if(_elements[j] < smallest) //If a smaller value is found, save
             {
-                smallest = _elements[j]; //Save smallest and smallest position
+                smallest = _elements[j]; //Save smallest value and smallest position
                 smallestPos = j;
 
-                //Print array here
-                printArray(_elements, _size, "//Found new smallest", i, smallestPos, j, 0, 0);
+                printArray(_elements, _size, "//Found new smallest", i-1, smallestPos, j, 0, 0); //Call function to display array
             }
         }
-        //swap values
-        tempInt = smallest;
+
+        tempInt = smallest; //swap values
         _elements[smallestPos] = _elements[i];
         _elements[i] = tempInt;
 
-        //Print array here
-        printArray(_elements, _size, "//Swap smallest to front of correct positions", i, smallestPos, 0, i, smallestPos);
+        printArray(_elements, _size, "//Swap", i, smallestPos, 0, i, smallestPos); //Call function to display array
     }
-
     return;
 }
 
 //Function used to output options to user and get input
 void menu(string _title, string _options[], int _size, int* _value)
 {
-    cout << "///// " << _title << " /////" << endl;
+    cout << "///// " << _title << " /////" << endl; //Print title and all of the options
     for(int i = 0; i < _size; i++)
     {
         cout << " >- [" << i+1 << "] " << _options[i] << endl;
     }
     cout << " >- [" << _size+1 << "] Quit" << endl;
 
-    inputInt(_size+1, 1, _value);
+    inputInt(_size+1, 1, _value); //Call function to get user input
 
-    //If user chooses quit, default to 0
-    if(*_value == _size+1)
+    if(*_value == _size+1) //If user chooses quit, default to 0
     {
         *_value = 0;
     }
@@ -146,7 +145,7 @@ void inputInt (int _maxLimit, int _minLimit, int* _value)
 //For printing out the array (green for correct, blue for current smallest, yellow for currently being checked, red 1 & 2 for being swapped)
 void printArray(int _elements[], int _elementsSize, string _message, int _green, int _blue, int _yellow, int _redOne, int _redTwo)
 {
-    HANDLE  hConsole; //For color, 1=blue, 2=green, 6=red, 4=yellow, 15=white
+    HANDLE  hConsole; //For color, 1=blue, 2=green, 6=yellow, 4=red, 15=white
     hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
 
     if(_message == " ") //NOT showing explanation mode
@@ -155,39 +154,45 @@ void printArray(int _elements[], int _elementsSize, string _message, int _green,
     }
     else //Showing explanation mode
     {
-        cout << " >- Pass [" << _green+1 << "]:"; //_green+1 is also the current pass number
+        if(_message != "//Swap") //If not swapping
+        {
+            cout << " >- Pass [" << _green+2 << "]:"; //_green+2 is the current pass number
+        }
+        else //Otherwise, when swapping
+        {
+            cout << " >- Pass [" << _green+1 << "]:"; //_green+1 is the current pass number
+        }
     }
 
-    for(int i = 0; i < _elementsSize; i++)
+    for(int i = 0; i < _elementsSize; i++) //Go through each element in the array
     {
         if(_message == " ") //if message does not need any explanation, print normally
         {
              cout << _elements[i] << " ";
         }
-        else //Otherwise add all of the explanations
+        else //Otherwise add all of the explanation color coding
         {
-            if(i <= _green && _message != "//Swap smallest to front of correct positions") //if before or equal to green marker, color green
-            {
-                SetConsoleTextAttribute(hConsole, 2);
-            }
-            else if(i == _blue && _message != "//Swap smallest to front of correct positions") //if i equals to blue marker, color blue
+            if(i == _blue && _message != "//Swap") //if i equals to blue marker, color blue
             {
                 SetConsoleTextAttribute(hConsole, 1);
             }
-            else if(i == _yellow && _message != "//Swap smallest to front of correct positions") //if i equals to yellow marker, color yellow
+            else if(i <= _green && _message != "//Swap") //if before or equal to green marker, color green
+            {
+                SetConsoleTextAttribute(hConsole, 2);
+            }
+            else if(i == _yellow && _message != "//Swap") //if i equals to yellow marker, color yellow
             {
                 SetConsoleTextAttribute(hConsole, 6);
             }
-            else if((i == _redOne || i == _redTwo) && _message == "//Swap smallest to front of correct positions") //if i equals to any red marker, color red
+            else if((i == _redOne || i == _redTwo) && _message == "//Swap") //if i equals to any red marker, color red
             {
                 SetConsoleTextAttribute(hConsole, 4);
             }
             cout << _elements[i] << " ";
             SetConsoleTextAttribute(hConsole, 15); //otherwise, reset to white
         }
-
     }
-    cout << "              " << _message << endl;
+    cout << "              " << _message << endl; //Print message
     return;
 }
 
@@ -204,10 +209,19 @@ void resetArray(int _array[], int _size)
 //Used for printing the color legend
 void printLegend()
 {
-    HANDLE  hConsole; //For color, 1=blue, 2=green, 6=red, 4=yellow, 15=white
+    HANDLE  hConsole; //For color, 1=blue, 2=green, 6=yellow, 4=red, 15=white
     hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
 
-    cout << ">- [LEGEND]: ";
-    SetConsoleTextAttribute(hConsole, 1);
+    cout << endl << ">- [LEGEND]: ";
+    SetConsoleTextAttribute(hConsole, 2); //Set colour to green
+    cout << "Already Sorted, ";
+    SetConsoleTextAttribute(hConsole, 1); //Set colour to blue
+    cout << "Current Smallest, ";
+    SetConsoleTextAttribute(hConsole, 6); //Set colour to yellow
+    cout << "Being Compared to Smallest, ";
+    SetConsoleTextAttribute(hConsole, 4); //Set colour to red
+    cout << "Being Swapped, ";
+    SetConsoleTextAttribute(hConsole, 15); //Set colour to white
 
+    return;
 }
