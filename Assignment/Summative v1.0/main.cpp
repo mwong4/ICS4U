f63c45cd19e993904d0c5c019b79a127da0d64be
Max <wong.max.c@gmail.com>
2021-03-23 10:07:11 -0400

    make project file for summative

---
+++ Assignment/Summative v1.0/main.cpp
@@ -0,0 +1,17 @@
+/*
+Author: Max Wong
+Date Created: Mar 9, 2020
+Date Updated: Mar 16, 2020
+Purpose: Final project for ICS4U
+Type: Main
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Hello world!" << endl;
+    return 0;
+}
